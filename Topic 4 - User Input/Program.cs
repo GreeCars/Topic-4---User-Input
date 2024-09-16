1ab5eb3ad9990cf16019f72ffc9443d85f2b44ca
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-16 09:38:13 -0400

    Working

--- Topic 4 - User Input/Program.cs
+++ Topic 4 - User Input/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string userName;
+
+Console.WriteLine("Enter username: ");
+userName = Console.ReadLine();
+
+Console.WriteLine("Your username is: " + userName);
+Console.ReadLine();
