38b97175a476bc7ee66e68a2067dab0aead8629f
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-17 09:25:07 -0400

    Working

--- Topic 4 - User Input/Program.cs
+++ Topic 4 - User Input/Program.cs
@@ -1,9 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string userName;
+//Console.WriteLine("Hello, World!");
+//string userName;
 
-Console.WriteLine("Enter username: ");
-userName = Console.ReadLine();
+//Console.WriteLine("Enter username: ");
+//userName = Console.ReadLine();
 
-Console.WriteLine("Your username is: " + userName);
-Console.ReadLine();
+//Console.WriteLine("Your username is: " + userName);
+//Console.ReadLine();
+
+//int age;
+//Console.WriteLine("Enter your age: ");
+//age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age));
+//Console.WriteLine("Your age is: " + age);
+//Console.ReadLine();
+
+double price;
+Console.WriteLine("What is the price?");
+Double.TryParse(Console.ReadLine(), out price);
+Console.WriteLine("The price is " + price.ToString("C"));
